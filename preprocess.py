bdc560f4d46b91d173219c70574da7ae82204e2f
amtuazon4 <amtuazon4@up.edu.ph>
2024-04-20 05:42:51 +0800

    finished Bag of words preprocessing function

--- preprocess.py
+++ preprocess.py
@@ -1,4 +1,5 @@
 # File for preprocessing
+
 import nltk
 import json
 from nltk.stem.porter import PorterStemmer
@@ -17,22 +18,43 @@ def tokenize(text):
 def lem(word):
     return stemmer.stem(word.lower())
 
+# Function for removing duplicates
+def rem_dup(arr):
+    return list(dict.fromkeys(arr))
+
 # Function for generating the bag of words
 def gen_BOW(json_data):
     bow = []
     for intent in json_data["intents"]:
         for text in intent["text"]:
-            for token in [lem(x) for x in tokenize(text)]:
+            for token in rem_dup([lem(x) for x in tokenize(text)]):
                 if(token not in bow): bow.append(token)
     return bow
 
+# Function for preprocessing the data using Bag of Words
+def BOW_preprocess(json_data, bow):
+    inp = []
+    out = []
+    for intent in json_data["intents"]:
+        for text in intent["text"]:
+            inp_row = rem_dup([lem(x) for x in tokenize(text)])
+            temp = [1 if(x in inp_row) else 0 for x in bow]
+            inp.append(temp)
+        out.append(intent)
+    return inp, out
 
+# Function for calculating tf_idf
+def tf_idf():
+    pass
 
-
-
+# Function for preprocessing the data using TF-IDF
+def TFIDF_preprocess(json_data, bow):
+    pass
 # Testing Area
 temp = read_json("init_data.json")
-gen_BOW(temp)
+bow = gen_BOW(temp)
+x, y = BOW_preprocess(temp, bow)
+print(len(x), len(y))
 
 # text = "I AM ATOMIC."
 # tokens = tokenize(text)
